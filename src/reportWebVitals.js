be19809204c2332d4217dc6fdd74e65129471b1e
Beatrice <bbeatricemwangi@gmail.com>
2025-01-14 20:22:35 +0300

    Initial commit with backend and frontend setup

---
+++ src/reportWebVitals.js
@@ -0,0 +1,9 @@
+// src/reportWebVitals.js
+import { reportWebVitals } from 'web-vitals';
+
+// You can keep the function to log the metrics if you want
+const logVitals = (metric) => {
+  console.log(metric);
+};
+
+export default logVitals;
